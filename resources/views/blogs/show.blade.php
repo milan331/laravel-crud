9f3055f82dbfe917bfad6903ae8159cb5b112bdd
milan katariya <mkataria331@gmail.com>
2021-07-20 18:16:16 +0530

    first commit

---
+++ resources/views/blogs/show.blade.php
@@ -0,0 +1,28 @@
+@extends('blogs.layout')
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2> Show blog</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('blogs.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+   
+    <div class="row">
+        <div class="col-xs-6 col-sm-6 col-md-6">
+            <div class="form-group">
+                <strong>Title:</strong>
+                {{ $blog->title }}
+            </div>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-6">
+            <div class="form-group">
+                <strong>Description:</strong>
+                {{ $blog->description }}
+            </div>
+        </div>
+    </div>
+@endsection
